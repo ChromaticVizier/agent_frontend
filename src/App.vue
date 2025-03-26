3f11984cdafb0e35648e00ad855bd51971ff3a7d
fuufhjn <zjq8947@gmail.com>
2025-03-26 19:00:51 +0800

    feat: initial commit.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="This is the test page for software engineering project."/>
   </div>
 </template>
 
